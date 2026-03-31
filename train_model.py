0ed58dce80eaa3bb2ab0cbe5b5866472ecb8f196
bob <bchr174@wgu.edu>
2026-03-30 23:27:50 -0600

    working on train_model

--- train_model.py
+++ train_model.py
@@ -13,7 +13,7 @@ from ml.model import (
     train_model,
 )
 
-project_path = r"\\wsl.localhost\Ubuntu-24.04\home\bob\Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+project_path = "/home/bob/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
