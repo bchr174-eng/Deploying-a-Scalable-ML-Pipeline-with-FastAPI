305a05a1e10f270404703abda2778227886cb5e2
bob <bchr174@wgu.edu>
2026-04-01 13:51:03 -0600

    working on unit tests

--- test_ml.py
+++ test_ml.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 #from sklearn.linear_model import LogisticRegression
 from ml.model import train_model, compute_model_metrics
 from ml.data import process_data
-from train_model import cat_features
+
 # TODO: implement the first test. Change the function name and input as needed
 def test_model_algorithm():
     """
@@ -29,13 +29,15 @@ def test_data_processing():
         'workclass': ['Private', 'Private', 'Self', 'Private'],
         'salary': ['<=50K', '>50K', '>50K', '<=50K']
     })
+
+
     # Mock label binning
     train, test = train_test_split(data, test_size=0.5, random_state=42)
     print("Training Sample:\n", train.head())
     X_train, y_train, _, _ = process_data(
         train, 
         label='salary',
-        categorical_features=cat_features
+        categorical_features=["workclass"]
     )
 
     
